da6cd46ecc0f7dc34225827de63619cfe361a8d0
Nate Bailey <nate@wintr.us>
2015-01-27 11:19:01 -0800

    Fetch user id with individual revisions

--- revisions.php
+++ revisions.php
@@ -4,12 +4,12 @@ include 'common.php';
 
 $query = <<<EOD
 SELECT g.page_id, g.page_title, g.page_namespace,
-  c.rev_id, c.rev_timestamp, c.rev_user_text,
+  c.rev_id, c.rev_timestamp, c.rev_user_text, c.rev_user,
   CAST(c.rev_len AS SIGNED) - CAST(IFNULL(p.rev_len, 0) AS SIGNED) AS byte_change
 FROM revision_userindex c
 LEFT JOIN revision_userindex p ON p.rev_id = c.rev_parent_id
 INNER JOIN page g ON g.page_id = c.rev_page
-WHERE g.page_namespace IN (0,4)
+WHERE g.page_namespace IN (0,2)
 AND c.rev_user_text IN ($sql_user_ids)
 AND c.rev_timestamp BETWEEN "$start" AND "$end"
 EOD;
