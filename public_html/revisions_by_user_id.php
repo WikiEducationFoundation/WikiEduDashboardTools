233a544e02495c0d9a08680508fa3df99ab894f9
Sage Ross <ragesoss@gmail.com>
2016-03-24 11:27:21 -0700

    Remove depecated revisions_by_user_id.php endpoint

--- public_html/revisions_by_user_id.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'common.php';
-require_once 'revisions.php';
-
-if (php_sapi_name() !== 'cli' ) {
-	echo_query_results(make_revisions_by_user_query());
-}
--- tests/bootstrap-phpunit.php
+++ tests/bootstrap-phpunit.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/../public_html/common.php';
 require_once __DIR__ . '/../public_html/articles.php';
 require_once __DIR__ . '/../public_html/revisions.php';
 require_once __DIR__ . '/../public_html/users.php';
-require_once __DIR__ . '/../public_html/revisions_by_user_id.php';
 
 class MockDbEscape {
 	public function escape_string($str) {
