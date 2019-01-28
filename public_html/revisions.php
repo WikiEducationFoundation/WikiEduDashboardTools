459ab8ae6db7dc78cbae35c03eea6ab0a36d6a53
Wes Reid <bwreid@gmail.com>
2019-01-28 14:40:12 -0800

    Replace spacing with tabs

--- public_html/revisions.php
+++ public_html/revisions.php
@@ -14,28 +14,28 @@ function make_revisions_by_user_query() {
 	if(isset($user_clause) && isset($namespaces)) {
 	  $query = "
 		SELECT g.page_id, g.page_title, g.page_namespace,
-		  c.rev_id, c.rev_timestamp, c.rev_user_text, c.rev_user,
-		  case when ct.ct_tag_id IS NULL then 'false' else 'true' end as system,
-		  case when c.rev_parent_id = 0 then 'true' else 'false' end as new_article,
-		  CAST(c.rev_len AS SIGNED) - CAST(IFNULL(p.rev_len, 0) AS SIGNED) AS byte_change
+			c.rev_id, c.rev_timestamp, c.rev_user_text, c.rev_user,
+			case when ct.ct_tag_id IS NULL then 'false' else 'true' end as system,
+			case when c.rev_parent_id = 0 then 'true' else 'false' end as new_article,
+			CAST(c.rev_len AS SIGNED) - CAST(IFNULL(p.rev_len, 0) AS SIGNED) AS byte_change
 		FROM revision_userindex c
 		LEFT JOIN revision_userindex p ON p.rev_id = c.rev_parent_id
 		INNER JOIN page g ON g.page_id = c.rev_page
 		LEFT JOIN change_tag_def ctd
-      ON ctd.ctd_name IN ($tags)
-    LEFT JOIN change_tag ct
-      ON ct.ct_rev_id = c.rev_id
-      AND ct.ct_tag_id = ctd.ctd_id
+			ON ctd.ctd_name IN ($tags)
+		LEFT JOIN change_tag ct
+			ON ct.ct_rev_id = c.rev_id
+			AND ct.ct_tag_id = ctd.ctd_id
 		WHERE g.page_namespace IN ($namespaces)
 		{$user_clause}
 		AND c.rev_timestamp BETWEEN $start AND $end
-	  ";
+		";
 	} else if(isset($sql_rev_ids)) {
 	  $query = "
 		SELECT rev_id, rev_page
 		FROM revision_userindex
 		WHERE rev_id IN ($sql_rev_ids)
-	  ";
+		";
 	}
 	return $query;
 }
