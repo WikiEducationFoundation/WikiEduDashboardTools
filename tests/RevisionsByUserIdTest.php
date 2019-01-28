95f7ccd8faee35964b6f59949e6ba46a0dd92ca9
Wes Reid <bwreid@gmail.com>
2019-01-28 14:40:12 -0800

    Replace spacing with tabs

--- tests/RevisionsByUserIdTest.php
+++ tests/RevisionsByUserIdTest.php
@@ -9,10 +9,10 @@ class RevisionsByUserTest extends PHPUnit_Framework_TestCase {
 		));
 		$expected = "
 			SELECT g.page_id, g.page_title, g.page_namespace,
-			  c.rev_id, c.rev_timestamp, c.rev_user_text, c.rev_user,
-			  case when ct.ct_tag_id IS NULL then 'false' else 'true' end as system,
-			  case when c.rev_parent_id = 0 then 'true' else 'false' end as new_article,
-			  CAST(c.rev_len AS SIGNED) - CAST(IFNULL(p.rev_len, 0) AS SIGNED) AS byte_change
+				c.rev_id, c.rev_timestamp, c.rev_user_text, c.rev_user,
+				case when ct.ct_tag_id IS NULL then 'false' else 'true' end as system,
+				case when c.rev_parent_id = 0 then 'true' else 'false' end as new_article,
+				CAST(c.rev_len AS SIGNED) - CAST(IFNULL(p.rev_len, 0) AS SIGNED) AS byte_change
 			FROM revision_userindex c
 			LEFT JOIN revision_userindex p ON p.rev_id = c.rev_parent_id
 			INNER JOIN page g ON g.page_id = c.rev_page
@@ -24,7 +24,7 @@ class RevisionsByUserTest extends PHPUnit_Framework_TestCase {
 			WHERE g.page_namespace IN (0,1,2,3,4,5,10,11,118,119)
 			AND c.rev_user_text IN ('Admin','北岛','Bak\'un')
 			AND c.rev_timestamp BETWEEN '20130101' AND '20170203'
-		  ";
+			";
 		$query = make_revisions_by_user_query();
 		$this->assertEquals(strip($expected), strip($query));
 	}
